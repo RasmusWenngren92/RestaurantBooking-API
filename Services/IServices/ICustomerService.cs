808b0fde8f7bba789a3d4fa70ec37cdcfb6131dc
Rasemouse <rasmuswenngren@hotmail.com>
2025-08-29 21:07:19 +0200

    Update ICustomerService to use specialized DTOs and add booking count method

--- Services/IServices/ICustomerService.cs
+++ Services/IServices/ICustomerService.cs
@@ -4,10 +4,11 @@ namespace RestaurantBookingAPI.Services.IServices
 {
     public interface ICustomerService
     {
-        Task<List<CustomerDTO>> GetAllCustomersAsync();
+        Task<IEnumerable<CustomerDTO>> GetAllCustomersAsync();
         Task<CustomerDTO?> GetCustomerByIdAsync(int customerId);
-        Task<int> AddCustomerAsync(CustomerDTO customerDTO);
-        Task<bool> UpdateCustomerAsync(CustomerDTO customerDTO);
+        Task<bool> AddCustomerAsync(CreateCustomerDTO customerDTO);
+        Task<bool> UpdateCustomerAsync(UpdateCustomerDTO customerDTO);
         Task<bool> DeleteCustomerAsync(int customerId);
+        Task<CustomerDTO?> GetCustomerWithBookingCountAsync(int customerId);
     }
 }
