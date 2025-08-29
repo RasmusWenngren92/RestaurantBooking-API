b2eda38aa34e4914177dfd886a19545d5289792c
Rasemouse <rasmuswenngren@hotmail.com>
2025-08-29 21:07:17 +0200

    Add AdminRegisterDTO, LoginAdminDTO and AuthResponseDTO

--- DTOs/AdminDTO.cs
+++ DTOs/AdminDTO.cs
@@ -1,6 +1,21 @@
 ﻿namespace RestaurantBookingAPI.DTOs
 {
-    public class AdminDTO
+    public class AdminRegisterDTO
     {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
     }
+    public class LoginAdminDTO
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+    public class AuthResponseDTO
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
+    }
+
 }
