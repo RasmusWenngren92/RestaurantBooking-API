4f0e2b7c2e1febf4967e9fc8279e3bf05d382d95
Rasemouse <rasmuswenngren@hotmail.com>
2025-08-29 21:07:17 +0200

    Remove unnecessary Display attributes from MenuItemDTO

--- DTOs/MenuItemDTO.cs
+++ DTOs/MenuItemDTO.cs
@@ -8,24 +8,19 @@ namespace RestaurantBookingAPI.DTOs
 
         [Required (ErrorMessage = "Name is required.")]
         [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
-        [Display(Name = "Menu Item Name")]
         public string? Name { get; set; }
         [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
-        [Display(Name = "Menu Item Description")]
         public string? Description { get; set; }
         [Required(ErrorMessage = "Price is required.")]
         [Range(0.01, double.MaxValue, ErrorMessage = "Price must be a positive value.")]
-        [Display(Name = "Price (sek)", Description = "Price in Swedish Krona (sek)")]
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
         [Required(ErrorMessage = "Category is required.")]
         [StringLength(50, ErrorMessage = "Category cannot exceed 50 characters.")]
-        [Display(Name = "Food Category", Description = "Type of cousine (e.g., Appetizer, Main Course)")]
         public string? Category { get; set; }
         [Display(Name = "Is Popular", Description = "Indicates if the menu item is popular")]
         public bool IsPopular { get; set; }
         [Url(ErrorMessage = "Invalid URL format.")]
-        [Display(Name = "Image URL", Description = "URL of the menu item image")]
         public string? ImageUrl { get; set; } 
         // Additional properties can be added as needed
     }
