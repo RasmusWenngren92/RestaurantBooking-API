96a4d013322e9ca43bc8bd9d5ae6d814ff80446b
Rasemouse <rasmuswenngren@hotmail.com>
2025-08-29 21:07:16 +0200

    Add Admin entity with username, password hash, email and role

--- Models/Entities/Admin.cs
+++ Models/Entities/Admin.cs
@@ -2,5 +2,11 @@
 {
     public class Admin
     {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+        public string Role { get; set; } = "Admin";
     }
 }
