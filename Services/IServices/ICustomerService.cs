87134a9ed123daca04aa3274a9174b7919f88518
Rasemouse <rasmuswenngren@hotmail.com>
2025-09-09 13:11:36 +0200

    Update service interfaces to accept ID parameters in method signatures

--- Services/IServices/ICustomerService.cs
+++ Services/IServices/ICustomerService.cs
@@ -7,7 +7,7 @@ namespace RestaurantBookingAPI.Services.IServices
         Task<IEnumerable<CustomerDTO>> GetAllCustomersAsync();
         Task<CustomerDTO?> GetCustomerByIdAsync(int customerId);
         Task<bool> AddCustomerAsync(CreateCustomerDTO customerDTO);
-        Task<bool> UpdateCustomerAsync(UpdateCustomerDTO customerDTO);
+        Task<bool> UpdateCustomerAsync(int id, UpdateCustomerDTO customerDTO);
         Task<bool> DeleteCustomerAsync(int customerId);
         Task<CustomerDTO?> GetCustomerWithBookingCountAsync(int customerId);
     }
--- Services/IServices/IMenuItemService.cs
+++ Services/IServices/IMenuItemService.cs
@@ -7,7 +7,7 @@ namespace RestaurantBookingAPI.Services.IServices
         Task<IEnumerable<MenuItemDTO>> GetAllMenuItemsAsync();
         Task<MenuItemDTO?> GetMenuItemByIdAsync(int menuId);
         Task<int> AddMenuItemAsync(MenuItemDTO menuItemDTO);
-        Task<bool> UpdateMenuItemAsync(MenuItemDTO menuItemDTO);
+        Task<bool> UpdateMenuItemAsync(int id, MenuItemDTO menuItemDTO);
         Task<bool> DeleteMenuItemAsync(int menuItemId);
     }
 }
